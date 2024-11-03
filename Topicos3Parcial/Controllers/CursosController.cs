cdabda807e8f70f589d894d7277e6ee3ed2aa12c
sebasthub <playandroidhb@gmail.com>
2024-11-03 14:04:08 -0300

    feat: criado cadastro de cursos

---
+++ Topicos3Parcial/Controllers/CursosController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjetoEnsalamento.Models;
+using Topicos3Parcial.Models;
+
+namespace Topicos3Parcial.Controllers
+{
+    public class CursosController : Controller
+    {
+        private AgendamentoDbContext db = new AgendamentoDbContext();
+
+        // GET: Cursos
+        public ActionResult Index()
+        {
+            return View(db.Cursos.ToList());
+        }
+
+        // GET: Cursos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Curso curso = db.Cursos.Find(id);
+            if (curso == null)
+            {
+                return HttpNotFound();
+            }
+            return View(curso);
+        }
+
+        // GET: Cursos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cursos/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome")] Curso curso)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cursos.Add(curso);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(curso);
+        }
+
+        // GET: Cursos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Curso curso = db.Cursos.Find(id);
+            if (curso == null)
+            {
+                return HttpNotFound();
+            }
+            return View(curso);
+        }
+
+        // POST: Cursos/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome")] Curso curso)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(curso).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(curso);
+        }
+
+        // GET: Cursos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Curso curso = db.Cursos.Find(id);
+            if (curso == null)
+            {
+                return HttpNotFound();
+            }
+            return View(curso);
+        }
+
+        // POST: Cursos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Curso curso = db.Cursos.Find(id);
+            db.Cursos.Remove(curso);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
