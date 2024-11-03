a5339f66660ff6e2b1f7530c00efc67bf3fdfc67
sebasthub <playandroidhb@gmail.com>
2024-11-03 00:25:41 -0300

    feat: criado views de unidade

---
+++ Topicos3Parcial/Controllers/UnidadesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjetoEnsalamento.Models;
+using Topicos3Parcial.Models;
+
+namespace Topicos3Parcial.Controllers
+{
+    public class UnidadesController : Controller
+    {
+        private AgendamentoDbContext db = new AgendamentoDbContext();
+
+        // GET: Unidades
+        public ActionResult Index()
+        {
+            return View(db.Unidades.ToList());
+        }
+
+        // GET: Unidades/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Unidade unidade = db.Unidades.Find(id);
+            if (unidade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(unidade);
+        }
+
+        // GET: Unidades/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Unidades/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Nome,Localização")] Unidade unidade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Unidades.Add(unidade);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(unidade);
+        }
+
+        // GET: Unidades/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Unidade unidade = db.Unidades.Find(id);
+            if (unidade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(unidade);
+        }
+
+        // POST: Unidades/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Nome,Localização")] Unidade unidade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(unidade).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(unidade);
+        }
+
+        // GET: Unidades/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Unidade unidade = db.Unidades.Find(id);
+            if (unidade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(unidade);
+        }
+
+        // POST: Unidades/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Unidade unidade = db.Unidades.Find(id);
+            db.Unidades.Remove(unidade);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
